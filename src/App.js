7ec1f5038f0d271b916fa7c77914a4ea528d84be
Dan Calise <calise@gmail.com>
2022-04-12 21:42:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello, I’m Patrick. A Senior Product Designer working at @Method in NY.
+        <br />
+        <br />
+        I’m passionate about shaping the next generation of meaningful digital
+        products and services.
+      </h1>
+      <h2>Get in Touch</h2>
+      <div className="card">Send me an Email -></div>
+      <h2>Other Things</h2>
+    </div>
+  );
+}
